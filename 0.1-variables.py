1450c6c1da973de32c9a71a79fc5c13ddfcaa3ed
Fatekiz <Fatekiz@users.noreply.github.com>
2024-05-06 16:45:57 -0400

    Ordene los códigos y solucione algunos problemas 1.0

--- 0.1-variables.py
+++ 0.1-variables.py
@@ -3,16 +3,15 @@
 name = "Benjamin"
 apellido = "Concha"
 edad = 18
-#2. impresion de la variable (print clasimo)
-print("Hola soy", name)
 
+#2. impresion de la variable (print clasimo)
+print("1. Hola soy", name)
 
 #3. Impresión de variable (print f-string)
-print(f"Hola mi nombre es {name}") 
-
+print(f"2. Hola mi nombre es {name}") 
 
 # 4. Impresión de variable (concatenación)
-print("1. ¡Hola buenas! yo soy " + name + " " + apellido  + " y tengo" + " " +str(edad) +" " + "años" )
+print("3. ¡Hola buenas! yo soy " + name + " " + apellido  + " y tengo" + " " + str(edad) +" " + "años" )
 
 #5. Declarando variable numérica
 edad = 18
@@ -23,7 +22,7 @@ n_comp = 5 + 4j
 n_comp2 = complex(5,4)
 
 #6. imprimiendo numeros
-
+print("4. mi edad es de: " + str(edad) + " años Y mi estatura es de: " + str(estatura) + " M")
 #Formateando la salida de números
 pi = 3,14159
 
