8f8dd7b6ab2d30f721fe7a4e3ff0576ce2be147a
Fatekiz <Fatekiz@users.noreply.github.com>
2024-05-07 14:49:20 -0400

    Solucione el problema con el pi, cambiando (,) por (.)

--- 0.1-variables.py
+++ 0.1-variables.py
@@ -16,19 +16,19 @@ print("3. ¡Hola buenas! yo soy " + name + " " + apellido  + " y tengo" + " " + 
 #5. Declarando variable numérica (Int/Float)
 edad = 18
 cumpleaños = 2005
-estatura = 1,71
+estatura = 1.71
 peso = 58.4
+
 #5.1 Declarando Números Complejos
 n_comp = 5 + 4j 
 n_comp2 = complex(5,4)
 
 #6. imprimiendo numeros
-print("4. mi edad es de: " + str(edad) + " años Y mi estatura es de: " + str(estatura) + " M")
+print("4. mi edad es de: " + str(edad) + " años Y mi estatura es de: " + str(estatura) + " M. " + "Y peso: " + str(peso) + " Kg.")
 
 #Formateando la salida de números
-Pi = 3,14159
-print("El valor de pi es {:.2f}" .format(Pi))
-
+pi = 3.14159
+print("El valor de pi es: ", round(pi,2))
 # 7. Variables en una sola linea (No es tan recomendado para depurar)
 pais, region, ciudad, codigo_postal = "Chile, ", "Los Lagos, ", "Queilén ", 5780000
 print("5. La informacion de Benjamin: ", pais, region, ciudad, codigo_postal )
