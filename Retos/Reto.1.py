3c30d150f5193d66590d517cc72182afbe808eee
Fatekiz <Fatekiz@users.noreply.github.com>
2024-05-08 16:35:01 -0400

    Cree la carpeta "retos" y acabo de subir el retoN1 a la plataforma.

---
+++ Retos/Reto.1.py
@@ -0,0 +1,11 @@
+#reto
+
+#solicitando edad y nombre
+nombre = input("¿Cual es tu nombre? ")
+edad = input("Ahora escribe tu edad actual tu edad actual: ")
+
+#realizando operación de suma para la variable edad
+suma = int(edad) + 20
+
+#imprimiendo nombre, edad, juntamente con la suma de la edad + 20 años.
+print("Hola, mi nombre es " + nombre + ", tengo " + edad + " años y en 20 años tendré " + str(suma) + " años." )
