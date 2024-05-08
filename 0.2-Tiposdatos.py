948931710f6a6c542f02fe620465880900a611b7
Fatekiz <Fatekiz@users.noreply.github.com>
2024-05-08 18:06:33 -0400

    Subí mis apuntes de la clase 08-05-24 sobre Tiposdatos

--- 0.2-Tiposdatos.py
+++ 0.2-Tiposdatos.py
@@ -1,3 +1,25 @@
-"""Hola
+#Tipos de datos
 
-"""
+
+# 1. STRINGS
+institucion = "Universidad de Los Lagos"
+asignatura = "Programación"
+descripcion = """Asignatura del primer semestre de la ingieneria 
+civil en informatica"""
+
+#imprimir la posición de un carácter
+print("1.-", institucion[1])
+
+#concatenacion
+resultado = print("2.-", institucion + " " +  asignatura)
+
+# Multiplicando el texto por un número
+print("3.-",asignatura *4)
+
+#División de cadena (split)
+
+print("4.-",institucion.split())
+
+#aplicando len()
+
+print("5.-", len(asignatura))
