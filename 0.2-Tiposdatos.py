287f052cc0e7087bf5b80bbafe661c1e0172b47c
Fatekiz <Fatekiz@users.noreply.github.com>
2024-05-13 20:18:51 -0400

    Terminé todos mis apuntes de la ultima clase de programación, y reforcé mi uso del f-strings.

--- 0.2-Tiposdatos.py
+++ 0.2-Tiposdatos.py
@@ -1,25 +1,80 @@
-#Tipos de datos
+#Importando libreria v
 
+import math
 
-# 1. STRINGS
-institucion = "Universidad de Los Lagos"
+print("####### TIPOS DE DATOS ######## \n") # "\n" --> un "Enter" o un espacio en blanco de separacion de parrafo cuando se imprima
+
+#1.- Números
+print("------- 1.- Números ------- \n")
+estatura = 1.71 # numero float
+peso = 58 # numero int
+
+# potencia (con 2 *) ej: 2**2 --> 2 elevado a 2
+imc = peso / (estatura **2)
+print(imc) #Imprimiendo el imc con la operación del elevado 
+print(math.trunc(imc)) # Primero llamar libreria (l3) y luego llamar funcion math.trunc(x)
+
+print(" ------- 2.- STRINGS -------\n")
+
+# 2. STRINGS
+institucion = "Universidad de Los Lagos" # toda variable "string" que yo asigne debe estar dentro de comillas (" x ") luego del"="
 asignatura = "Programación"
 descripcion = """Asignatura del primer semestre de la ingieneria 
-civil en informatica"""
+civil en informatica"""  #Aquí son 3 comillas por lado ya que usamos mas de una linea para la assignacion ("""x""")
 
-#imprimir la posición de un carácter
-print("1.-", institucion[1])
+#imprimir la posición de un carácter con los corchetes ejemplo print(variable[0] 0 primera letra, 1 segunda y -1 la ultima
+print("2 -", institucion[0])
+print(f"2.1 - {institucion[1]}") # ocupando el " f- string" encuentro que es la mejor manera para el "print" ej:(print(f "x" {y}) ) 
+print(f"2.2 - {institucion[1]}") # y las llaves "{}" son para encapsular y llamar la variable dentro del print(f "y" {x})
 
 #concatenacion
-resultado = print("2.-", institucion + " " +  asignatura)
+resultado = print("2.3 -", institucion + " " +  asignatura) # la concatenación une las variables con un "+"
 
 # Multiplicando el texto por un número
-print("3.-",asignatura *4)
+print("2.4 -",asignatura *4) # multiplicar una variable por un numero es posible, y esto genera una repetición de la variable según la mult.
 
 #División de cadena (split)
 
-print("4.-",institucion.split())
+print("2.5 -",institucion.split()) # la funcion (x.split) hace que separe cada palabra por separada de la variable
 
 #aplicando len()
 
-print("5.-", len(asignatura))
+print("2.6 -", len(asignatura)) # la funcion ( len(x) ) imprime la cantidad de letras (incluyendo espacios) que contiene la variable "x"
+
+print("------- 3.- BOOLEANOS -------")
+
+#. 3 BOOLEANOS
+
+ampolleta = False # acá le asignamos a una variable "false" o "true" 
+interruptor = True
+
+print(f"3 -{interruptor,ampolleta}") # y acá cuando imnprimamos la variable nos arrojará el valor asignado del bool (si es False/True)
+
+#Salida Booleanas
+
+print(f"3.1 -{1 <= 0 }") # con esto hacemos una pregunta que si lo solicitado está correcto. Esta correcto por ende arroja "False"
+print(f"3.2 -{100 > 90}") # nos arrojará un ("True" si es correcto y un "False" si no lo es)
+print(f"3.3 -{19 == 19}") # COMENTARIO: "=" --> asignación | "==" --> es de comparación (Matemáticas).
+print(f"3.4 -{bool(0)}") # en booleanos existe el true/false 1/0 | este es 0 == false
+print(f"3.5 -{bool(1)}") # 1 == True.
+
+print(f" ------- impresión de clasificación de variables -------")
+
+# impresión de clasificación de variables.
+print(f"clas. {type(ampolleta)}") # la función type(x) hace que nos diga a que clase pertenece la variable ej. int, float, bool, str etc...
+print(f"clas. {type(asignatura)}")
+print(f"clas. {type(peso)}")# COMENTARIO -- la funcion "f -string" la f debe estar pegada al print(f) y pegada al siguiente paréntesis
+print(f"clas. {type(imc)}")
+
+
+print(" ------- LISTAS -------")
+
+# 4.- Listas | Las "listas" son como cajas donde podemos guardar cosas (variables) en ellas, y luego llamarlas o buscarlas cuando las necesitamos  
+ciudades = ["castro", "Queilen", "Ancud", "Quellón", "Chonchi"]  
+varios = ["Nicolas", 20, True] 
+print(f"clas. {type(ciudades)}") # Acá nos reconoce que la variable "ciudades" es de tipo "list"
+print(f"4 - {ciudades}")
+print(f"4.1 - {varios}")
+
+#4.1.- forma formal de crear lista "list()" | Nadie la usa xD
+list(["python", "Ruby"])
