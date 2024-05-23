ebedae298eb17ea82248032cfa5f5e21b20cfe5f
Fatekiz <Fatekiz@users.noreply.github.com>
2024-05-23 11:47:01 -0400

    Cree una retroalimentación después de ver la manera del progfe, y aprendí a que es mejor llamar las variables.

---
+++ Algoritmos/Algoritmo_N2.1.py
@@ -0,0 +1,11 @@
+# Despúes de ver el ejercicio hecho por el profe intente hacerlo así, y luego solo llamar las variables.
+
+lapiz = "Solo existen 2 tipos de colores, rojo y azul"
+menos50 = (len(lapiz) <= 50)
+caracter_10 = lapiz[ :10]
+
+# imprimir resultados
+print(f"La descripción del articulo >lapiz< es: {lapiz} \n")
+print(f"La drescripción del articulo >lapiz< tiene {len(lapiz)} carácteres. \n")
+print(f"La descripción del articulo >lapiz< ¿Tiene menos de 50 carácteres? {menos50} \n")
+print(f"Los primeros 10 carácteres de la descripción son: {caracter_10} \n")
