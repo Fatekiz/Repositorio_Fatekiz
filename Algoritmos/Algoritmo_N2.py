261953b3756fca7aed4326cea174e344fc55067c
Fatekiz <Fatekiz@users.noreply.github.com>
2024-05-23 11:47:38 -0400

    Solo termine de redactar la petición del ejercicio

--- Algoritmos/Algoritmo_N2.py
+++ Algoritmos/Algoritmo_N2.py
@@ -3,10 +3,11 @@ Se necesita desarrollar un código que permita ingresar una descripción breve d
 de librería. Por ejemplo: “Este lápiz solo tiene formato de color azul”. Se solicita realizar las
 siguientes operaciones:
 
+no tener mas de 50 carcteres
+que imprima si es verdadero q tiene menos de 50
+imprimir las primeras 10 palabras
 """
 lapiz = "Solo hay 2 colores de lápices, rojo y azul"
-print(len(lapiz))
-
 
 f = (len(lapiz) <= 50)
 print(f)
