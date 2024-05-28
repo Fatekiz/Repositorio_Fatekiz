be2b8143a517ecaea6ee655e6f15a85baef32990
Fatekiz <Fatekiz@users.noreply.github.com>
2024-05-28 14:33:02 -0400

    agregue imprimir y input

---
+++ Comandos/Comandos_Generales.py
@@ -0,0 +1,11 @@
+# COMANDOS QUE HE APRENDIDO
+x = "Variable para prueba"
+
+# 1. Imprimir 
+print("prueba") # <-- Funciona para imprimir el mensaje que yo quiera en la consola, este debe estar seguido de ("con el mensaje")
+print(f"prueba, {x}") # <-- Personalmente prefiero usar el f-string ya que para mi me es más comodo si debo usar una variable usando "{}"
+
+# INPUT
+
+input() # <-- Con esto podemos solicitar un dato al usuario, el cual lo debe escribir en la consola 
+variable_segun_input = input("ingresa un dato") # <-- Con esto inmediatamente 
