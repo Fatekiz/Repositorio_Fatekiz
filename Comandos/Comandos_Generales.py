f4ad4a4a23412eeb77041fb990b153dd071b9acb
Fatekiz <168034457+Fatekiz@users.noreply.github.com>
2024-06-07 10:05:20 -0400

    añadi funciones para los iterables.

--- Comandos/Comandos_Generales.py
+++ Comandos/Comandos_Generales.py
@@ -8,7 +8,7 @@ nombre = "benjamin"
 nombre_completo = "Benjamin Concha"
 
 
-# 2. TRANSFORMAR A UN TIPO DE DATO
+#                        2. TRANSFORMAR A UN TIPO DE DATO
 int() # num entero
 float() # num flotante
 complex() # num complejo
@@ -16,11 +16,11 @@ str() # string o cadena
 bool() # booleano (0,1)
 
 
-# 1. IMPRIMIR """Con prueba """
+#                         1. IMPRIMIR """Con prueba """
 print("prueba") # <-- Funciona para imprimir el mensaje que yo quiera en la consola, este debe estar seguido de ("con el mensaje")
 print(f"prueba, {x}") # <-- Personalmente prefiero usar el f-string ya que para mi me es más comodo si debo usar una variable usando "{}"
 
-# INPUT
+#                            INPUT
 
 input() # <-- Con esto podemos solicitar un dato al usuario, el cual lo debe escribir en la consola 
 
@@ -30,13 +30,13 @@ variable_segun_input = input("ingresa un dato") # <-- Con esto inmediatamente la
 # print(f"Llamar variable {Variable_segun_input}")
 
 
-# OPERACIONES MATEMATICAS
+#                                OPERACIONES MATEMATICAS
 round # redondea un numero 
 
 """ Prueba """
 round(y,2) # Redondea a num. entero la variable que yo encierre en () despues del round según la cantidad de decimales que yo eliga despues de la ","
 
-# SECUENCIAS
+#                                      SECUENCIAS
 
 list # <-- crea lista
 tuple # <-- crea tuplas
@@ -49,7 +49,7 @@ Ciudades = ["Queilen" , "Castro", "Queilen", "Santiago"]
 Ciudades_Santiago = ["Puente alto", "La Florida"]
 Rango_numerico = list(range(0,11)) # <-- asi tendra 10 numeros dentro (cuenta desde el 0) por ende si quiero llegar hasta 10 debo usar el "(x,11)".
 
-# OPERACIONES 
+#                                   OPERACIONES 
 
 len # imprime la cantidad de elementos o carácteres
 min # imprime el valor minimo
@@ -110,7 +110,7 @@ print(nombrem)
 print(nombre_completo.split()) # Separo la cadena de la variable por " , "
 
 
-# BUCLES
+#                               BUCLES
 
 while x == x: 
 
@@ -119,11 +119,51 @@ while x == x:
 if x == x:
     x = 10
 
+#                         Listas
+
+# append() <--- agrega un elemento al final de la lista
+# insert() <--- inserta un elemento en un lugar específico de la lista
+# extend() <--- combina 2 o más listas
+# remove() <--- elimina la primera ocurrencia de un elemento (en caso de que este duplicado)
+# pop() <--- Borra y retorna el último elemento de una lista
+# sort() <--- ordena los elementos de la lista de manera ascendete
 
 
-#                            FUNCIONES SETS
+
+#                              TUPLAS (tuple)
+
+# count() <--- imprime el total de repeticiones que tiene el elemento seleccionado dentro de la tupla
+# index() <--- imprime el indice con el elemento seleccionado en la tupla
+# sorted() <--- Entrega una lista ordenada con los valores de la tupla
+# min() <--- devuelve el valor minimo de la tupla
+# max() <--- devuelve el valor máximo de la tupla
+# sum() <--- imprime la suma de los valores dentro de la tupla
+
+
+
+
+
+#                             SETS
 
 # add() <- añade elementos al set seleccionado
-# clear() <- 
-# discard () <-
+# clear() <- elimina todos los elementos del set
+# discard () <--- elimina un elemento del set
 # copy() <- crea una copia del set 
+# intersection() <--- devuelve un solo set que contiene solo los valores que esetan en ambos sets
+# difference() <--- a diferencia del intersection devuelve un set que contiene los elementos que estan en el set 1 pero no en el 2
+# pop() <--- eliminar un archivo del set
+# remove() <--- eliminar la primeria ocurrencia de un elemento dentro del set
+# len() <--- cuenta cantidad de elementos dentro del set
+
+
+#                   DICCIONARIOS
+
+# keys()
+# values()
+# items()
+# clear()
+# copy()
+# pop() <-- elimina la clave y valor (llamando la clave)
+
+#             ---- Listas ----
+# sort() <-- Lo ordena 
