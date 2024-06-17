370f182ae420653db2ee51d3f67347b9cfc4e8ab
Fatekiz <168034457+Fatekiz@users.noreply.github.com>
2024-06-17 14:03:54 -0400

    Lo haré como lo hizo el profe

--- Retos/Reto.3.py
+++ Retos/Reto.3.py
@@ -3,8 +3,3 @@
 ica = [134, 99, 245, 50]
 ciudades = ["santiago", "temuco", "osorno", "punta arenas" ]
 
-ciudades_ica = list(zip(ciudades,ica))
-
-print(ciudades_ica)
-
-print(sorted(ciudades_ica))
