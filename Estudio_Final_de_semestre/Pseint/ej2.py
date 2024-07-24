8331a05ec8d13327df1b5ceb59e27b5af3d3494c
Fatekiz <168034457+Fatekiz@users.noreply.github.com>
2024-07-24 15:12:55 -0400

    Terminado

--- Estudio_Final_de_semestre/Pseint/ej2.py
+++ Estudio_Final_de_semestre/Pseint/ej2.py
@@ -3,19 +3,21 @@ la primera vale 20% | la segunda vale 60% | la tercera y cuarta un 10%
 luego el programa imprime si el estudiante aprobo o no tomando un 4.0 como nota de corte
 """
 
-print("Ingrese las 4 notas obtenidas para generar un promedio ponderado: \n")
+print(" \n Ingrese las 4 notas obtenidas para generar un promedio ponderado: \n")
 
-nota1 = input("Nota N°1: ")
-nota2 = input("Nota N°2: ")
-nota3 = input("Nota N°3: ")
-nota4 = input("Nota N°4: ")
+nota1 = float(input("Nota N°1: "))
+nota2 = float(input("Nota N°2: "))
+nota3 = float(input("Nota N°3: "))
+nota4 = float(input("Nota N°4: "))
 # calculo
 
 nota_final= (nota1 * 0.2) + (nota2 * 0.6) + (nota3 * 0.1) + (nota3 * 0.1)
 
-if nota_final >4.0:
+if nota_final >=4.0:
     
     print(f"Usted ha aprobado con un: {nota_final}")
 
 else:
     print(f"Usted ha reprobado con un: {nota_final}")
+
+# Ej terminado! :D 
